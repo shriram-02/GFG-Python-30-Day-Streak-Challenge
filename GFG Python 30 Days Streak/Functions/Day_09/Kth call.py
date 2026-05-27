6f0e99cad08b8c221d9ad02db24a13a195f3a0bc
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-05-27 15:05:34 -0400

    Delete GFG Python 30 Days Streak/Functions/Day_09 directory

--- GFG Python 30 Days Streak/Functions/Day_09/Kth call.py
+++
@@ -1,9 +0,0 @@
-# declare the variable count here
-count = 0
-
-def utility():
-    # access the global variable count
-    global count  
-    # increases the value of count by 1
-    count += 1
-    print(count, end=" ")
--- GFG Python 30 Days Streak/Functions/Day_09/Sum Of Digits.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sumOfDigits(self, n):
-        # code here
-        s = 0
-        while n > 0:
-            s += n % 10
-            n //= 10
-        return s
